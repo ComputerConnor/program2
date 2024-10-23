7662fed533c8641e8f93637e0fd8c3a60a7faab7
afhudson42 <afhudson42@tntech.edu>
2024-10-23 15:29:59 -0500

    Initial code for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+using namespace std;
 
+void printMenu();
+
+int main() {
+    int userInput;
+
+    do {
+        //Prints the menu to the user.
+        printMenu();
+        cin >> userInput;
+
+        if(userInput < 1 || userInput > 5) {
+            cout << "\n\nPlease choose a number 1-5. " << endl;
+        }
+        
+        switch(userInput){
+            case 1:
+
+            break;
+            
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+
+            break;
+
+
+        }
+    } while(userInput < 1 || userInput > 5);
+
+
+    return 0;
+}
+
+void printMenu() {
+
+    cout << "Welcome to Techtarget!" << endl;
+    cout << "How could we help you today? " << endl;
+    cout << "1.) Display stock. " << endl;
+    cout << "2.) Add item to cart. " << endl;
+    cout << "3.) Remove item from cart. " << endl;
+    cout << "4.) Checkout " << endl;
+}
