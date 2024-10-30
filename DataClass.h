82166c7bb04c5dde0205d7ca12ec59629ae129f3
Connor Jordan <56240039+ComputerConnor@users.noreply.github.com>
2024-10-30 14:02:58 -0500

    Create DataClass.h

---
+++ DataClass.h
@@ -0,0 +1,39 @@
+#ifndef DATACLASS_H
+#define DATACLASS_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class DataClass {
+private:
+    string itemName;
+    double price;
+    string category;
+
+public:
+    DataClass(const string& name = "", double cost = 0.0, const string& type = "")
+        : itemName(name), price(cost), category(type) {}
+
+    void setItem(const string& name, double cost, const string& type) {
+        itemName = name;
+        price = cost;
+        category = type;
+    }
+
+    string getItemName() const { return itemName; }
+    double getPrice() const { return price; }
+    string getCategory() const { return category; }
+
+    void print() const {
+        cout << "Item: " << itemName << ", Price: $" << price << ", Category: " << category << endl;
+    }
+
+    // Get details as a formatted string
+    string getDetails() const {
+        return "Item: " + itemName + ", Price: $" + to_string(price) + ", Category: " + category;
+    }
+};
+
+#endif
